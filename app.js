081bcd8c0c041432bfc2bcea367bb752f8c2e0c8
Mikhail Goloto <goloto@pm.me>
2020-02-26 20:53:51 +0300

    trying fix

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const app = express();
 
 app.get('/', function(req, res) {
     res.send('hello world');
+    //console.log('somebody send GET to 3000 port')
 });
 
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+    //console.log('Example app listening on port 3000!');
 });
