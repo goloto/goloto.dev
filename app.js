ce18f4b59b1d99f8e8deafb3c7700b3a4e962702
Mikhail Goloto <goloto@pm.me>
2020-02-26 21:56:11 +0300

    fixes again

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
 
+const port = process.env.PORT;
+if (port == null || port == "") {
+    port = 8000;
+}
+app.listen(port);
+
 app.get('/', function(req, res) {
     res.send('hello world');
     console.log('somebody send GET to 3000 port')
 });
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
-});
+app.listen(process.env.PORT);
