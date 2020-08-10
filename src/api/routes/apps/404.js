e536bd552757e90071024a6329b48c62715042c6
Mikhail Goloto <goloto@pm.me>
2020-08-10 14:41:33 +0300

    merge test into master

--- src/api/routes/apps/404.js
+++ src/api/routes/apps/404.js
@@ -1,21 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const notFoundRouter = express.Router();
-const supermanNums = require('../../../services/superman');
 
 notFoundRouter
-    .get("/why", function (req, res) {
-        res.send("It's 100% serious randomly research");
-    })
-    .use((req, res) => {
-        res
-            .status(404)
-            .render("404", {
-                h1: "Page not found",
-                title: "Page not found",
-                nums: supermanNums,
-            });
-    });
+	.get("/why", function (req, res) {
+		res.send("It's 100% serious randomly research");
+	})
+	.use((req, res) => {
+		res
+			.status(404)
+			.render("404", {
+				h1: "Page not found",
+				title: "Page not found",
+			});
+	});
 
 router.use(notFoundRouter);
 
