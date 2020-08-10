6fd39f74b16616c7c67cab272321a2f6412d592a
Mikhail Goloto <goloto@pm.me>
2020-08-10 15:04:06 +0300

    fix Procfile

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -4,14 +4,14 @@ const appsRouter = require('./apps/index');
 const timeline = require('../../models/timeline');
 
 router
-    .get('/', (req, res) => {
-        res.render('index', {
-            title: 'Mikhail Goloto and web-dev magic',
-            timeline: timeline
-        });
-        console.log('Someone open main page at: ', Date.now());
-    })
-    .use('/apps', appsRouter);
+	.get('/', (req, res) => {
+		res.render('index', {
+			title: 'Mikhail Goloto and web-dev magic',
+			timeline: timeline
+		});
+		console.log('Someone open main page at: ', Date.now());
+	})
+	.use('/apps', appsRouter);
 
 module.exports = router;
 
