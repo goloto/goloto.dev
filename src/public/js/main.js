9a772a9bc1facac886a84e6b50494f8b99519e13
Mikhail Goloto <goloto@pm.me>
2020-09-16 19:20:09 +0300

    small fixes in main.js structure

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -76,103 +76,103 @@ function colorSwitcher() {
 
 		setTimeout(run, interval);
 	}, interval);
-}
 
-function getNewColor(previewColor) {
-	let colors = {
-		main: [
-			'rgb(88, 143, 152)',
-			'rgb(28, 93, 103)',
-			'rgb(41, 71, 107)',
-			'rgb(94, 109, 184)',
-			'rgb(158, 76, 149)',
-			'rgb(135, 58, 97)',
-			'rgb(133, 74, 214)',
-			'rgb(194, 2, 94)',
-			'rgb(255, 137, 126)',
-			'rgb(81, 135, 255)',],
-		hover: [
-			'rgb(68, 123, 132)',
-			'rgb(8, 73, 83)',
-			'rgb(21, 51, 87)',
-			'rgb(74, 89, 164)',
-			'rgb(138, 56, 129)',
-			'rgb(115, 38, 77)',
-			'rgb(113, 54, 194)',
-			'rgb(174, 0, 74)',
-			'rgb(235, 117, 106)',
-			'rgb(61, 115, 235)',],
-		doubleHover: [
-			'rgb(48, 103, 112)',
-			'rgb(0, 53, 63)',
-			'rgb(1, 31, 67)',
-			'rgb(54, 69, 144)',
-			'rgb(118, 36, 109)',
-			'rgb(95, 18, 57)',
-			'rgb(93, 34, 174)',
-			'rgb(154, 0, 54)',
-			'rgb(215, 97, 86)',
-			'rgb(41, 95, 215)',],
-	};
-	let colorNumber;
+	function getNewColor(previewColor) {
+		let colors = {
+			main: [
+				'rgb(88, 143, 152)',
+				'rgb(28, 93, 103)',
+				'rgb(41, 71, 107)',
+				'rgb(94, 109, 184)',
+				'rgb(158, 76, 149)',
+				'rgb(135, 58, 97)',
+				'rgb(133, 74, 214)',
+				'rgb(194, 2, 94)',
+				'rgb(255, 137, 126)',
+				'rgb(81, 135, 255)',],
+			hover: [
+				'rgb(68, 123, 132)',
+				'rgb(8, 73, 83)',
+				'rgb(21, 51, 87)',
+				'rgb(74, 89, 164)',
+				'rgb(138, 56, 129)',
+				'rgb(115, 38, 77)',
+				'rgb(113, 54, 194)',
+				'rgb(174, 0, 74)',
+				'rgb(235, 117, 106)',
+				'rgb(61, 115, 235)',],
+			doubleHover: [
+				'rgb(48, 103, 112)',
+				'rgb(0, 53, 63)',
+				'rgb(1, 31, 67)',
+				'rgb(54, 69, 144)',
+				'rgb(118, 36, 109)',
+				'rgb(95, 18, 57)',
+				'rgb(93, 34, 174)',
+				'rgb(154, 0, 54)',
+				'rgb(215, 97, 86)',
+				'rgb(41, 95, 215)',],
+		};
+		let colorNumber;
 
-	if (previewColor !== undefined) {
-		colorNumber = getRandomInt(0, colors.main.length);
+		if (previewColor !== undefined) {
+			colorNumber = getRandomInt(0, colors.main.length);
 
-		while (colorNumber === previewColor) {
+			while (colorNumber === previewColor) {
+				colorNumber = getRandomInt(0, colors.main.length);
+			}
+		} else {
 			colorNumber = getRandomInt(0, colors.main.length);
 		}
-	} else {
-		colorNumber = getRandomInt(0, colors.main.length);
+
+		return {
+			main: colors.main[colorNumber],
+			hover: colors.hover[colorNumber],
+			doubleHover: colors.doubleHover[colorNumber]
+		};
 	}
 
-	return {
-		main: colors.main[colorNumber],
-		hover: colors.hover[colorNumber],
-		doubleHover: colors.doubleHover[colorNumber]
-	};
-}
+	function changeElementsColor(color) {
+		let additionalStyles = document.head.appendChild(document.createElement("style"));
+		let toChangeElements = document.querySelectorAll("*[class*='to-change']");
 
-function changeElementsColor(color) {
-	let additionalStyles = document.head.appendChild(document.createElement("style"));
-	let toChangeElements = document.querySelectorAll("*[class*='to-change']");
+		additionalStyles.innerHTML =
+			'.main .personal .contacts-list__item::before {color: ' + color.main + '}' +
+			'.main .timeline__item:hover .timeline__line .circle_tech {background-color: ' + color.main + '; transition: background-color 0.2s ease-in}' +
+			'.header__menu .menu__item:hover {background-color:' + color.hover + '}' +
+			'.header__menu .menu__item .pop-up-menu .pop-up-menu__item:hover {background-color:' + color.doubleHover + '}' +
+			'.main ul.projects .project:first-child .project__line {background: linear-gradient(aliceblue, ' + color.main + ', ' + color.main + ')}' +
+			'.main ul.projects .project:last-child .project__line {background: linear-gradient(' + color.main + ', ' + color.main + ', aliceblue)}';
 
-	additionalStyles.innerHTML =
-		'.main .personal .contacts-list__item::before {color: ' + color.main + '}' +
-		'.main .timeline__item:hover .timeline__line .circle_tech {background-color: ' + color.main + '; transition: background-color 0.2s ease-in}' +
-		'.header__menu .menu__item:hover {background-color:' + color.hover + '}' +
-		'.header__menu .menu__item .pop-up-menu .pop-up-menu__item:hover {background-color:' + color.doubleHover + '}' +
-		'.main ul.projects .project:first-child .project__line {background: linear-gradient(aliceblue, ' + color.main + ', ' + color.main + ')}' +
-		'.main ul.projects .project:last-child .project__line {background: linear-gradient(' + color.main + ', ' + color.main + ', aliceblue)}';
+		toChangeElements.forEach((element) => {
+			let className = element.className.baseVal === undefined ? element.className : element.className.baseVal;
 
-	toChangeElements.forEach((element) => {
-		let className = element.className.baseVal === undefined ? element.className : element.className.baseVal;
-
-		switch (className.substring(className.indexOf('to-change_') + 'to-change_'.length)) {
-			case 'fill':
-				element.style.fill = color.main;
-				break;
-			case 'color':
-				element.style.color = color.main;
-				break;
-			case 'color-hover':
-				element.style.color = color.hover;
-				break;
-			case 'background':
-				element.style.backgroundColor = color.main;
-				break;
-			case 'background-hover':
-				element.style.backgroundColor = color.hover;
-				break;
-			case 'border':
-				element.style.borderColor = color.main;
-				break;
-			case 'start-timeline':
-				element.style.background = 'linear-gradient(0deg, ' + color.main + ' 0%, aliceblue 100%)';
-				break;
-			case 'end-timeline':
-				element.style.background = 'linear-gradient(0deg, aliceblue 0%, ' + color.main + ' 100%)';
-				break;
-		}
-	})
+			switch (className.substring(className.indexOf('to-change_') + 'to-change_'.length)) {
+				case 'fill':
+					element.style.fill = color.main;
+					break;
+				case 'color':
+					element.style.color = color.main;
+					break;
+				case 'color-hover':
+					element.style.color = color.hover;
+					break;
+				case 'background':
+					element.style.backgroundColor = color.main;
+					break;
+				case 'background-hover':
+					element.style.backgroundColor = color.hover;
+					break;
+				case 'border':
+					element.style.borderColor = color.main;
+					break;
+				case 'start-timeline':
+					element.style.background = 'linear-gradient(0deg, ' + color.main + ' 0%, aliceblue 100%)';
+					break;
+				case 'end-timeline':
+					element.style.background = 'linear-gradient(0deg, aliceblue 0%, ' + color.main + ' 100%)';
+					break;
+			}
+		})
+	}
 }
